bf7cb2538fcd458b35f7b0c55a473eb29ece239a
Chandra <chandra.chaganti@hotmail.com>
2020-07-01 15:19:22 +0530

    hotdog: Import PopupCamera
    credit to lineage and aospa

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Camera
+PRODUCT_PACKAGES += \
+    OnePlusCameraHelper
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
