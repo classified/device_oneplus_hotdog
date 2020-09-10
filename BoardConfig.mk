c865a7df3596cd6ed34398b9f02a266d27a62ad1
chandra <chandra.chaganti@hotmail.com>
2020-09-10 09:48:32 +0200

    hotdog: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from oneplus sm8150-common
+-include device/oneplus/sm8150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/oneplus/hotdog
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Display
+TARGET_SCREEN_DENSITY := 560
+
+# Fingerprint
+SOONG_CONFIG_ONEPLUS_MSMNILE_FOD_POS_X = 610
+SOONG_CONFIG_ONEPLUS_MSMNILE_FOD_POS_Y = 2618
+SOONG_CONFIG_ONEPLUS_MSMNILE_FOD_SIZE = 220
+
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
+# inherit from the proprietary version
+-include vendor/oneplus/hotdog/BoardConfigVendor.mk
