7e06a4bffd531ffacf566ecd2127b8526b0ff505
chandra <chandra.chaganti@hotmail.com>
2020-09-11 14:48:51 +0200

    hotdog: seperate device specific Partitions,Recovery,A/B and Ramdisk

--- device.mk
+++ device.mk
@@ -35,6 +35,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
 
+# A/B
+AB_OTA_PARTITIONS += \
+    product \
+    vbmeta_system
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
@@ -44,6 +49,14 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+#Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from oneplus sm8150-common
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
